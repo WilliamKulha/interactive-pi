b408b054617f3b7b703aef10bf7564f58e4b198e
WilliamKulha <wkulha14@gmail.com>
2019-04-11 16:34:00 -0400

    inital commit of a finished project.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import '../src/sass/styles.scss';
+import '../src/assets/images/fav-icon.png';
+import '@fortawesome/fontawesome-free/js/all';
+import $ from 'jquery';
+
+$('#j-nav').on('click','a', function(event){ 
+  event.preventDefault();
+  let dest = $(this).attr("href");
+  if (dest === '#') {
+    window.scrollTo(0,0);
+  } else {
+    let elementOffset =  $(dest).offset();   
+    let navHeight = elementOffset.top - 65;
+    window.scrollTo(0, navHeight);
+  }   
+});
+
+if (navigator.userAgent.match(/OS X.*Safari/) && ! navigator.userAgent.match(/Chrome/)) {
+    document.getElementById('j-banner-header').className += 'safari';
+}
